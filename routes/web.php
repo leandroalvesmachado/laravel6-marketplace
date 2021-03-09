3a7e538bcd93bb174f4dc168d4a548c0816a168e
leandroalvesmachado <leandroalvesmachado@gmail.com>
2021-03-08 22:30:51 -0300

    Fim da aula 38

--- routes/web.php
+++ routes/web.php
@@ -37,4 +37,62 @@ Route::get('/model', function () {
     // $user->update([
     //     'name' => 'Mass Update'
     // ]);
+
+    // Como eu faria para recuperar a loja de um usuário
+    // $user = \App\User::find(4);
+    // return $user->store;
+
+    // Recuperar os produtos de uma loja
+    // $loja = \App\Store::find(1);
+    // return $loja->products;
+    // return $loja->products()->where('product_id', 1)->get();
+
+    // Recuperar os produtos de uma categoria
+    // $categoria = \App\Category::find(1);
+    // return $categoria->products;
+
+    // Criar uma loja para um usuário
+    // $user = \App\User::find(10);
+    // $store = $user->store()->create([
+    //     'name' => 'Loja Teste',
+    //     'description' => 'Loja Teste de produtos de informática',
+    //     'phone' => '85-XXXXXXXX',
+    //     'mobile_phone' => '85-XXXXXXXX',
+    //     'slug' => 'loja-teste'
+    // ]);
+
+    // Criar um produto para uma loja
+    // $store = \App\Store::find(41);
+    // $product = $store->products()->create([
+    //     'name' => 'Notebook Dell',
+    //     'description' => 'CORE I5 16 GB',
+    //     'body' => 'Teste',
+    //     'price' => 2999.90,
+    //     'slug' => 'notebook-dell'
+    // ]);
+
+    // Criar uma categoria
+    // \App\Category::create([
+    //     'name' => 'Games',
+    //     'description' => null,
+    //     'slug' => 'games'
+    // ]);
+
+    // \App\Category::create([
+    //     'name' => 'Notebooks',
+    //     'description' => null,
+    //     'slug' => 'notebooks'
+    // ]);
+
+    // Adicionar um produto para uma categoria ou vice-versa
+    // $product = \App\Product::find(49);
+    // vinculando a uma ou mais categorias pelo id da categoria
+    // $product->categories()->attach([1]);
+    // remove o vinculo
+    // $product->categories()->detach([1]);
+
+    // adiciona ou vincula as categorias automaticamente
+    // inteligente para saber quando remover ou adicionar
+    // realiza a sincronização
+    // $product->categories()->sync([1, 2]);
 });
